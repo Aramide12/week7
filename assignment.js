3e757b3cae7b86642e469c54010725d5a8156750
Aramide12 <Abiodunaramide58@gmail.com>
2024-03-05 13:59:55 +0100

    feat:assignment

--- assignment.js
+++ assignment.js
@@ -1,38 +1,108 @@
-const arr1 = [12,34,76,123,56,8,2,988,90,345]
+const arr = [12,34,76,123,56,8,2,988,90,345]
 
+//sum of all the numbers in the array
+function sumArr(arr){
+return arr.reduce((a,b)=>a+b,0)
+}
 
-max = Math.max(...arr1)
-min = Math.min(...arr1)
-divisible = arr1.filter(num => num % 2 === 0).length
-age = arr1.length
-greater = arr1.filter(num => num > 50)
-order = arr1.slice().sort((a,b) => a - b);
-even = arr1.every(num => num % 2 === 0)
-const index = arr1.indexOf(76)
-order2 = arr1.slice().reverse()
+//largest number in the array
+function largestNum(arr){
+return Math.max(...arr)
+}
 
-console.log(max)
-console.log(min)
-console.log(divisible)
-console.log(age)
-console.log(greater)
-console.log(order)
-console.log(even)
-console.log(index)
-console.log(order2)
+function largeNum2(arr){
+    return arr.sort((a,b))
+}
+function smallNum(arr){
+return arr.sort((a,b)=>a-b)[0]
+}
+
+//divisible by 2
+function even(arr){
+return arr.filter(num => num % 2 == 0).length + `numbers`
+}
+
+//average
+function avgArr(arr){
+const len = arr.length
+const sum =(arr.reduce((q,w)=>q+w,0))
+return (sum/len).toFixed(2)
+}
+
+//new array with numbers greater than 50
+function fifty(arr){
+    return arr.filter (num =>num > 50)
+}
+
+//even numbers in the array
+function isEven(arr){
+    return arr.map(num => num % 2 == 0 ? 'is Even' : 'isOdd')
+}
+
+//index of the number 76
+function anyNum(arr, num){
+    return arr.indexOf(num)
+}
+
+console.log(isEven(arr))
 
 
 const names = ["Alice", "Bob", "Charlie", "Darlington", "Emma", "Fiona", "George", "Hannah", "Joy", "Julia"]
 
-names.sort((a,b)=> b.length - a.length)
-
-const upper = names => names.replace(names[0], names[0].toUpperCase())
-
-const replace = names => names.replace('Alice', 'Eleanor')
-
-const concat = names=> names.concat(...names)
-
-const indexOf = names => names.indexOf('Emma')
-
-console.log(names)
+//sum of all the names in the array
 console.log(names.length)
+
+//is "charlie" included in the array
+function findname(names){
+    return (names.includes('Charlie'));
+}
+
+//longest name in the array
+function longestNm(names){
+    return names.sort((a,b)=>b.length - a.length)[0]
+}
+
+//shortest name in the array
+function shortNm(names){
+    return names.sort((a,b) => a.length - b.length)[0]
+}
+
+//new array returning all the names in uppercase letters
+function newArr (names){
+    return names.map(a => a.toUpperCase())
+}
+
+//sort the names in alphabetical order
+function order (names){
+    return names.sort()
+}
+
+//check if all array contains five character
+function five(names){
+    return names.filter(names.length < 5)
+}
+
+//replace alice with eleanor
+function add(names){
+    return names.map(names => names === 'Alice' ? 'Eleanor' : names )
+}
+
+//index of emma
+function emma(names){
+    return names.indexOf ('Emma')
+}
+
+//concatenate all names into single string separated by ,
+function con(names){
+    return names.join(0)
+}
+
+console.log(findname(names))
+console.log(longestNm(names))
+console.log(shortNm(names))
+console.log(newArr(names))
+console.log(order(names))
+console.log(five(names))
+console.log(add(names))
+console.log(emma(names))
+console.log(con(names))
